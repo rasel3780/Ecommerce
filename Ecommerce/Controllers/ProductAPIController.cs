b2909ffdae1a4fc9f7ae5f44c891419b831f8ca6
Rasel Kibria <rasel5jkibria@gmail.com>
2024-04-26 21:14:49 +0600

    Product list showing in card with add to card button

--- Ecommerce/Controllers/ProductAPIController.cs
+++ Ecommerce/Controllers/ProductAPIController.cs
@@ -30,6 +30,49 @@ namespace Ecommerce.Controllers
             product.Quantity = 45;
             productsList.Add(product);
 
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
+
+            product = new Product();
+            product.Name = "Laptop-3";
+            product.Picture = "https://img.freepik.com/free-vector/screen-tv-mockup_1053-198.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714003200&semt=sph";
+            product.Price = 52000;
+            product.Quantity = 45;
+            productsList.Add(product);
+
 
             return Ok(productsList);
         }
