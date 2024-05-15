0c85a5e10fb7e84ceb67f6d5e6126f742505d0aa
Rasel Kibria <rasel5jkibria@gmail.com>
2024-05-16 02:05:01 +0600

    Product add to cart counting with model working

--- Ecommerce/wwwroot/js/Controller/ProductController.js
+++ Ecommerce/wwwroot/js/Controller/ProductController.js
@@ -8,13 +8,13 @@
                 productContent += `
                     <div class="col-md-3 mb-4">
                         <div class="card h-100">
-                            <img src="${value.picture}" class="card-img-top" alt="${value.name}">
+                            <img id='pdPicture_${index}' src="${value.picture}" class="card-img-top" alt="${value.name}">
                             <div class="card-body text-center">
-                                <h5 class="card-title">${value.name}</h5>
-                                <p class="card-text">Price: ${value.price}</p>
-                                <button class="btn btn-primary">
+                                <h5 id='pdName_${index}' class="card-title">${value.name}</h5>
+                                <p id='pdPrice_${index}' class="card-text">Price: ${value.price}</p>
+                                <a id='btnPdAddToCart_${index}' class="btn btn-primary" onclick='ProductController.AddToCart(this)'>
                                     <i class="fas fa-shopping-cart"></i> Add to Cart
-                                </button>
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -23,6 +23,41 @@
 
             $('#productContainer').html(productContent);
         })
+
+    },
+
+    AddToCart: (cntrl) => {
+       
+        var LstCartProducts = [];
+
+        if (localStorage.getItem("LstCartProducts") != undefined && localStorage.getItem("LstCartProducts") != null) {
+            LstCartProducts = localStorage.getItem("LstCartProducts");
+        }
+
+        console.log("----Existing Product-----");
+        console.log(LstCartProducts);
+        var targetIndex = $(cntrl).attr("id").split('_')[1];
+        var targetImage = $('#pdPicture_' + targetIndex).attr('src');
+        var targetName = $('#pdName_' + targetIndex).html();
+        var targetPrice = $('#pdPrice_' + targetIndex).html();
+
+        $('#lblCartCnt').html(parseInt($('#lblCartCnt').html()) + 1);
+
+        var targetProduct = {
+            Image: targetImage,
+            Name: targetName,
+            Price: targetPrice
+        }
+
+        var LstCartProductsNew = [];
+
+        LstCartProductsNew.push(targetProduct);
+        localStorage.setItem("LstCartProducts", LstCartProductsNew);
+
+        console.log("----After adding Product-----");
+        console.log(LstCartProductsNew);
+
+        alert("Porduct Added");
 
     }
 }
