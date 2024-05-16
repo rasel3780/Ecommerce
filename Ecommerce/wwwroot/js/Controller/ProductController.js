bcaeedb2f68aa2d539cffd957977fae7881db69c
Rasel Kibria <rasel5jkibria@gmail.com>
2024-05-16 22:33:21 +0600

    dummy json data added

--- Ecommerce/wwwroot/js/Controller/ProductController.js
+++ Ecommerce/wwwroot/js/Controller/ProductController.js
@@ -1,16 +1,17 @@
-﻿var ProductController = {
+﻿var LstCartProducts = [];
+var ProductController = {
     LstProducts: () => {
         //data have analysis, so need call back function
 
         ProductService.LstProducts(function (response) {
             let productContent = '';
-            $.each(response, function (index, value) {
+            $.each(response.products, function (index, value) {
                 productContent += `
                     <div class="col-md-3 mb-4">
                         <div class="card h-100">
-                            <img id='pdPicture_${index}' src="${value.picture}" class="card-img-top" alt="${value.name}">
+                            <img id='pdPicture_${index}' src="${value.thumbnail}" class="card-img-top" alt="${value.title}">
                             <div class="card-body text-center">
-                                <h5 id='pdName_${index}' class="card-title">${value.name}</h5>
+                                <h5 id='pdName_${index}' class="card-title">${value.title}</h5>
                                 <p id='pdPrice_${index}' class="card-text">Price: ${value.price}</p>
                                 <a id='btnPdAddToCart_${index}' class="btn btn-primary" onclick='ProductController.AddToCart(this)'>
                                     <i class="fas fa-shopping-cart"></i> Add to Cart
@@ -28,14 +29,14 @@
 
     AddToCart: (cntrl) => {
        
-        var LstCartProducts = [];
+       /* var LstCartProducts = [];*/
 
-        if (localStorage.getItem("LstCartProducts") != undefined && localStorage.getItem("LstCartProducts") != null) {
-            LstCartProducts = localStorage.getItem("LstCartProducts");
-        }
+        //if (localStorage.getItem("LstCartProducts") != undefined && localStorage.getItem("LstCartProducts") != null) {
+        //    LstCartProducts = localStorage.getItem("LstCartProducts");
+        //}
 
-        console.log("----Existing Product-----");
-        console.log(LstCartProducts);
+        //console.log("----Existing Product-----");
+        //console.log(LstCartProducts);
         var targetIndex = $(cntrl).attr("id").split('_')[1];
         var targetImage = $('#pdPicture_' + targetIndex).attr('src');
         var targetName = $('#pdName_' + targetIndex).html();
@@ -49,13 +50,13 @@
             Price: targetPrice
         }
 
-        var LstCartProductsNew = [];
+        //var LstCartProductsNew = [];
 
         LstCartProductsNew.push(targetProduct);
-        localStorage.setItem("LstCartProducts", LstCartProductsNew);
+        //localStorage.setItem("LstCartProducts", LstCartProductsNew);
 
-        console.log("----After adding Product-----");
-        console.log(LstCartProductsNew);
+        //console.log("----After adding Product-----");
+        //console.log(LstCartProductsNew);
 
         alert("Porduct Added");
 
